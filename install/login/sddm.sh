f9d2e44351830b504ecff62fb984e18a19e7f8c8
cederron <alex.gs.80@gmail.com>
2025-11-26 23:06:34 +0100

    initial

--- install/login/sddm.sh
+++ install/login/sddm.sh
@@ -2,10 +2,6 @@ sudo mkdir -p /etc/sddm.conf.d
 
 if [ ! -f /etc/sddm.conf.d/autologin.conf ]; then
   cat <<EOF | sudo tee /etc/sddm.conf.d/autologin.conf
-[Autologin]
-User=$USER
-Session=hyprland-uwsm
-
 [Theme]
 Current=breeze
 EOF
