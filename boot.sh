85a67317ed54dba39ec476d7a887a763efc586b0
cederron <alex.gs.80@gmail.com>
2025-11-26 23:08:39 +0100

    fix repo dir

--- boot.sh
+++ boot.sh
@@ -27,7 +27,7 @@ rm -rf ~/.local/share/omarchy/
 git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
 
 # Use custom branch if instructed, otherwise default to master
-OMARCHY_REF="${OMARCHY_REF:-master}"
+OMARCHY_REF="${OMARCHY_REF:-kde}"
 if [[ $OMARCHY_REF != "master" ]]; then
   echo -e "\e[32mUsing branch: $OMARCHY_REF\e[0m"
   cd ~/.local/share/omarchy
